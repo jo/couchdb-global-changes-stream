ba677b5d0f8df30afc7fbd2a7556e32465f9e056
Johannes J. Schmidt <jo@die-tf.de>
2016-06-15 17:00:58 +0200

    test: console logs for debugging

--- test.js
+++ test.js
@@ -8,10 +8,15 @@ var dbname = 'test-global-changes-stream'
 var couch = nano(COUCHDB)
 
 test('existing database and single change', function (t) {
+  console.log('start test...')
   couch.db.destroy(dbname, function () {
+    console.log('create db...')
     couch.db.create(dbname, function () {
+      console.log('start feed...')
       feed(COUCHDB, function (stream) {
+        console.log('feed started...')
         stream.on('data', function (change) {
+          console.log('on data', change)
           if (change.db_name !== dbname) return
 
           t.equal(change.change.seq, 1)
@@ -28,23 +33,23 @@ test('existing database and single change', function (t) {
   })
 })
 
-test('newly created database and single change', function (t) {
-  couch.db.destroy(dbname, function () {
-    feed(COUCHDB, function (stream) {
-      stream.on('data', function (change) {
-        if (change.db_name !== dbname) return
-
-        t.equal(change.change.seq, 1)
-        t.equal(change.change.id, 'mydoc')
-
-        stream.stop()
-      })
-
-      stream.on('end', t.end)
-
-      couch.db.create(dbname, function () {
-        couch.use(dbname).insert({ _id: 'mydoc', foo: 'bar' })
-      })
-    })
-  })
-})
+// test('newly created database and single change', function (t) {
+//   couch.db.destroy(dbname, function () {
+//     feed(COUCHDB, function (stream) {
+//       stream.on('data', function (change) {
+//         if (change.db_name !== dbname) return
+//
+//         t.equal(change.change.seq, 1)
+//         t.equal(change.change.id, 'mydoc')
+//
+//         stream.stop()
+//       })
+//
+//       stream.on('end', t.end)
+//
+//       couch.db.create(dbname, function () {
+//         couch.use(dbname).insert({ _id: 'mydoc', foo: 'bar' })
+//       })
+//     })
+//   })
+// })
