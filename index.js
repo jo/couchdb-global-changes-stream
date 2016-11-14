43791bfa769a158d21e1610e64ecdc26ea44aa28
Johannes J. Schmidt <jo@die-tf.de>
2016-11-15 00:17:13 +0100

    restart db updates feed

--- index.js
+++ index.js
@@ -19,13 +19,41 @@ function isNoSystemDb (change) {
 }
 
 function getDbUpdates (couch, options) {
-  return couch({
-    url: '_db_updates',
-    qs: {
-      feed: 'continuous',
-      timeout: options.timeout
+  var since
+
+  return _(function (push, next) {
+    var requestOptions = {
+      url: '_db_updates',
+      qs: {
+        feed: 'continuous',
+        timeout: options.timeout
+      }
     }
+
+    if (since) {
+      requestOptions.qs.since = since
+    }
+
+    couch(requestOptions)
+      .on('data', push.bind(null, null))
+      .on('error', push)
+      .on('end', function () {
+        // wait a bit for the parse and filter pipeline to finish
+        // to ensure `since` has been set
+        setTimeout(next, 100)
+      })
   })
+  .split()
+  .filter(isNotEmptyLine)
+  .map(parseJSON)
+  .map(function (data) {
+    if (data.last_seq) {
+      since = data.last_seq
+    }
+    return data
+  })
+  .filter(isUpdate)
+  .filter(isNoSystemDb)
 }
 
 function formatChange (dbname) {
@@ -60,15 +88,10 @@ module.exports = function (url, options) {
 
   options = options || {}
   // options.timeout = options.timeout || 60000
-  options.timeout = options.timeout || 100
+  options.timeout = options.timeout || 1000
 
   return _.pipeline(
     getDbUpdates(couch, options),
-    _.split(),
-    _.filter(isNotEmptyLine),
-    _.map(parseJSON),
-    _.filter(isUpdate),
-    _.filter(isNoSystemDb),
     _.map(getChanges(couch)),
     _.sequence()
   )
